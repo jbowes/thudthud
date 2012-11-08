f4ea9817ea7dc2e04cb0d8e64a24174ae2b7dcd8
James Bowes <jbowes@repl.ca>
2012-11-08 18:36:17 -0400

    basic sequencer functionality

--- static/thudthud.js
+++ static/thudthud.js
@@ -1,9 +1,47 @@
-console.log("HI THERE");
+var bar = 0;
+var beats = {};
+
+function playBeats() {
+    $("th").each(function(i) {
+        if (i == bar) {
+            $(this).addClass("active");
+            $(this).removeClass("inactive");
+        } else {
+            $(this).removeClass("active");
+            $(this).addClass("inactive");
+        }
+
+    });
+
+    $.each(beats, function(beat) {
+        $(beats[beat]).each(function(i) {
+            if (bar == i && beats[beat][i] == true) {
+                console.log(beat + " " + bar);
+                $("audio#snd-" + beat).get(0).play();
+            }
+        });
+    });
+
+    window.setTimeout(playBeats, 256);
+    bar++;
+    if (bar == 8) {
+        bar = 0;
+    }   
+}
 
 jQuery(function() {
     $("td").click(function(event) {
         drum_type = $(this).parent().attr("id");
         console.log("clicked: " + drum_type + " " + event.target.id);
         $("audio#snd-" + drum_type).get(0).play();
+
+        if (!(drum_type in beats)) {
+            beats[drum_type] = [false, false, false, false, false, false, false, false];
+        }
+        beats[drum_type][event.target.id] = true;
+
+        $(this).addClass("active");
     });
+
+   playBeats();
 });
