65e7a07b8e1972d0c7c0cbdd03613cac9269c987
James Bowes <jbowes@repl.ca>
2012-11-09 09:41:28 -0400

    Persist beats table state to redis

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 var fileserve = require('./fileserve');
 var model = require('./model');
 
+var redis = require("redis");
 var express = require('express');
 var http = require('http');
 
 var app = express();
 
 var table = model.initModel();
+
+auth = {
+    redisHost: "slimehead.redistogo.com",
+    redisPort: 9328,
+    redisPass: process.env.REDIS_PASS,
+};
+
+var redisClient = redis.createClient(auth.redisPort, auth.redisHost);
+redisClient.auth(auth.redisPass);
+
+// try and load the persisted beats table state
+redisClient.on("ready", function() {
+    redisClient.get("beats", function(err, reply) {
+        if (reply) {
+            console.log("found persisted table: " + reply);
+            table = JSON.parse(reply);
+        }
+    });
+});
 
 fileserve.configure(app, table);
 
@@ -21,6 +41,7 @@ io.sockets.on('connection', function (socket) {
 
     socket.on('toggle', function(data) {
         table.table[data.drum][data.bar] = data.state;
+        redisClient.set("beats", JSON.stringify(table));
         socket.broadcast.emit('toggle', data);
     });
 });
